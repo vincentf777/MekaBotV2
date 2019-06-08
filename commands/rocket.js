390f55101b2788e36f169bb418d3747e144f64f6
Chara <37601555+lightlyss@users.noreply.github.com>
2019-06-08 16:58:15 -0400

    reconfigure rocket since the images have different res

--- commands/rocket.js
+++ commands/rocket.js
@@ -3,23 +3,29 @@ const jimp = require("jimp");
 const request = require("request");
 
 module.exports = function(meka, msg, args) {
-	const UNIT_SIZE = 150, R_WIDTH = 80;
+	const UNIT_SIZE = 150, R_WIDTH = 100, R_HEIGHT = 108;
+	const AVA_PATH = "./images/avatar.png";
+	const R_PATH = "./images/sources/rockets.png";
+	const DVA_PATH = "./images/sources/dva.png";
+	const RES_PATH = "./images/final.png"
+
 	request(msg.mentions.users.first().displayAvatarURL)
-		.pipe(fs.createWriteStream("./images/avatar.png"))
+		.pipe(fs.createWriteStream(AVA_PATH))
 		.on("close", async () => {
-			let dva = await jimp.read("./images/dva.png");
-			let rocket = await jimp.read("./images/rockets.png");
-			let ava = await jimp.read("./images/avatar.png");
+			let dva = await jimp.read(DVA_PATH);
+			dva.resize(UNIT_SIZE, UNIT_SIZE);
+			let rocket = await jimp.read(R_PATH);
+			let ava = await jimp.read(AVA_PATH);
 			ava.resize(UNIT_SIZE, UNIT_SIZE);
 			let randInt = Math.floor(Math.random() * 5 + 2);
 			let bg = new jimp(UNIT_SIZE + randInt*R_WIDTH + UNIT_SIZE, UNIT_SIZE);
 
 			bg.composite(dva, 0, 0);
 			for (var i = 0; i < randInt; i++) {
-				bg.composite(rocket, UNIT_SIZE + i*R_WIDTH, 0);
+				bg.composite(rocket, UNIT_SIZE + i*R_WIDTH, UNIT_SIZE/2 - R_HEIGHT/2);
 			}
 			bg.composite(ava, UNIT_SIZE + randInt*R_WIDTH, 0);
-			bg.write("./images/final.png");
-			msg.channel.send({file: "./images/final.png"});
+			bg.write(RES_PATH);
+			msg.channel.send({file: RES_PATH});
 		});
 }
